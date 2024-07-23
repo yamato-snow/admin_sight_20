704ec833f659eae9bb89a1470d60e1c396d5e9dc
Yosho98325 <brreakin_ksyk_325@yahoo.co.jp>
2024-07-24 06:34:54 +0900

    standardとguestalkのページを見られるようにした

--- apps/regular/views.py
+++ apps/regular/views.py
@@ -17,13 +17,34 @@ class ListTaskView(LoginRequiredMixin, ListView):
 class StandardTaskView(LoginRequiredMixin, CreateView):
     template_name = 'regular/task_standard.html'
     model = Standard
+    fields = ['title', 'zoom', 'user']
 
 class GuestalkTaskView(LoginRequiredMixin, CreateView):
     template_name = 'regular/task_guestalk.html'
     model = Guestalk
+    fields = ['title', 'day', 'vol', 'guest', 'guest_url', 'theme', 'comment', 'template', 'thumbnail', 'spreadsheet', 'zoom', 'user']
 
 def index_view(request):
-    return render(
-        request,
-        'regular/index.html',
-        )
+    task_list = Task.objects.all()
+    standard_list = Standard.objects.all()
+    guestalk_list = Guestalk.objects.all()
+    
+    paginator_task = Paginator(task_list, 10)  # 10 items per page
+    paginator_standard = Paginator(standard_list, 10)  # 10 items per page
+    paginator_guestalk = Paginator(guestalk_list, 10)  # 10 items per page
+    
+    page_number_task = request.GET.get('task_page')
+    page_number_standard = request.GET.get('standard_page')
+    page_number_guestalk = request.GET.get('guestalk_page')
+    
+    page_obj_task = paginator_task.get_page(page_number_task)
+    page_obj_standard = paginator_standard.get_page(page_number_standard)
+    page_obj_guestalk = paginator_guestalk.get_page(page_number_guestalk)
+    
+    context = {
+        'page_obj_task': page_obj_task,
+        'page_obj_standard': page_obj_standard,
+        'page_obj_guestalk': page_obj_guestalk,
+    }
+    
+    return render(request, 'regular/index.html', context)
