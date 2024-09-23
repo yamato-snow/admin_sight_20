b32bcc0836518bf8dbbf67e4604baf54d51aa2c0
yamato-snow <yamato2016baseball@icloud.com>
2024-09-24 07:21:19 +0900

    ログイン機能実装完了

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.regular.apps.RegularConfig',  # 変更
+    'apps.accounts.apps.AccountsConfig'
     # 'apps.book',
 ]
 
