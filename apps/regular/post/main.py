361bacb3ccb122e3ee7724000cb92388f03a1189
Yosho98325 <brreakin_ksyk_325@yahoo.co.jp>
2024-10-03 06:59:50 +0900

    DBの値を通常開催の投稿に反映できるようになった。

--- apps/regular/post/main.py
+++ apps/regular/post/main.py
@@ -1,21 +1,36 @@
+# 必要なモジュールのインポート
+import os
+import sys
+import time
+import django
+
+# Django の環境を設定（プロジェクトのルートパスに変更してください）
+sys.path.append('/Users/yoshino/Local/06_20_manager/admin_site/admin_sight_20')
+# 現在の作業ディレクトリを取得し、必要なパスを追加
+#root_directory = os.getcwd()
+#sys.path.append(os.path.join(root_directory, 'admin_sight_20'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_sight_20.settings')
+
+# Django のセットアップ
+django.setup()
+
+# Django モデルのインポート
+from apps.regular.models import Standard
+
 # src ディレクトリ内のファイルをインポート
 from src.webdriver_setup import setup_webdriver
 from src.config_reader import read_config
 from src.post_standard import PostStandard
 from src.post_guest import PostGuest
 from src.post_guest_pre import PostGuestPre
-# その他、必要なモジュールをインポート
-import os
-import time
-import sys
 
 def main():
     # 構成ファイルにユーザー名、パスワード、カテゴリ URL などの必要な詳細が含まれている
     root_directory = os.getcwd()
-    
     config_file_path = os.path.join(root_directory, 'apps', 'regular', 'post', 'config', 'config.txt')
     config = read_config(config_file_path)
-    
+
     driver = setup_webdriver()
 
     trigger = sys.argv[1]
@@ -37,7 +52,16 @@ def main():
 
         # チャットへ投稿文を送信
         if trigger == 'std':
-            post1.post('ここにひとこと記入', '⚪︎⚪︎', '6月21日')
+            # Standard モデルからデータを取得
+            standard_data = Standard.objects.get(pk=1)  # IDが1のデータを取得
+
+            # データベースの comment1 と template1 を使用して post1.post を呼び出す
+            comment = standard_data.comment1
+            theme = standard_data.template1
+
+            # 取得したデータを使用して投稿
+            post1.post(comment, theme, '6月21日')
+
         elif trigger == 'gst':
             post1.post('回', 'ここにひとこと記入', 'https://libecity.com/user_profile/pJaiOmsWiWV62BR9GeydWww1Nqf2', 'ゲスト名', 'トーク内容', '⚪︎⚪︎', '6月21日')
         else:
